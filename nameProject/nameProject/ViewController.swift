af4b4846e80d67066692c3e8706fa3aa72885d13
user <user@LeoMac08.local>
2021-12-03 09:05:11 +0100

    push nameProject

---
+++ nameProject/nameProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  nameProject
+//
+//  Created by user on 03.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
